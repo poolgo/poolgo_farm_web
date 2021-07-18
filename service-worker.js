81f4cbab05df8ae3a6588825fe3200ade5596a06
bytego20 <86067274+bytego20@users.noreply.github.com>
2021-07-18 22:54:35 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+importScripts("https://cdn.myun.info/workbox-v4.3.1/workbox-sw.js", "precache-manifest.1805631c5e8071a59f0008ace3846290.js");
+
+// workbox.precaching([
+//     // 注册成功后要立即缓存的资源列表
+// ])
+// //
+
+// //
+
+// self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+
+
+// vue-cli3.0 supports pwa with the help of workbox-webpack-plugin, we need to get the precacheing list through this sentence.
+//
+workbox.core.skipWaiting(); // 强制等待中的 Service Worker 被激活
+workbox.core.clientsClaim(); // Service Worker 被激活后使其立即获得页面控制权
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+workbox.routing.registerRoute(
+    new RegExp('.*\.html'),
+    workbox.strategies.networkOnly()
+);
+workbox.routing.registerRoute(
+    new RegExp('.*\.(?:js|css)'),
+    workbox.strategies.networkOnly()
+);
+workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.networkOnly()
+);
